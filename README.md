58cd4a8d7e645aff7637275151c26aeed28d7537
Luke Lee <luke0201@naver.com>
2021-04-17 11:30:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Titanic Competition
+
+This is my Kaggle submission code.
+
+## Requirements
+
+You need Python 3.6 or higher version.
+
+## Usage
+
+Download the dataset using the following command.
+
+```
+kaggle competitions download -c titanic
+unzip titanic.zip && rm titanic.zip
+```
+
+Then run `titanic.py` as follows.
+
+```
+python titanic.py
+```
+
+Finally, submit the result using the following command. Replace the `submission_message` by yours.
+
+```
+kaggle competitions submit -c titanic -f submission.csv -m <submission_message>
+```
---
+++ titanic.py
@@ -0,0 +1,84 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import sys
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+
+
+def load_data(path):
+    df = pd.read_csv(path)
+
+    return df
+
+
+def split_feature_label(Xy):
+    y = Xy['Survived']
+    X = Xy.drop('Survived', axis=1, inplace=False)
+    return X, y
+
+
+def transform(X):
+    X.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+
+    X['Age'].fillna(X['Age'].mean(), inplace=True)
+    X['Embarked'].fillna('N', inplace=True)
+    X['Fare'].fillna(X['Fare'].mean(), inplace=True)
+
+    string_features = ['Sex', 'Embarked']
+    for feature in string_features:
+        label_encoder = LabelEncoder()
+        X[feature] = label_encoder.fit_transform(X[feature])
+
+    return X
+
+
+def train_model(X, y):
+    param_grid = {
+        'max_depth': [2, 3, 5, 10],
+        'min_samples_split': [2, 3, 5],
+        'min_samples_leaf': [1, 5, 8]
+    }
+    decision_tree_classifier = DecisionTreeClassifier()
+    grid_search_cv = GridSearchCV(decision_tree_classifier, param_grid=param_grid, scoring='accuracy', cv=5)
+
+    grid_search_cv.fit(X, y)
+
+    return grid_search_cv.best_estimator_
+
+
+def parse_args():
+    parser = ArgumentParser(
+        description='Generate the submission file for Kaggle Titanic competition.')
+    parser.add_argument(
+        '--train', type=Path, default='train.csv',
+        help='path of train.csv downloaded from the competition')
+    parser.add_argument(
+        '--test', type=Path, default='test.csv',
+        help='path of test.csv downloaded from the competition')
+
+    return parser.parse_args()
+
+
+def main(args):
+    Xy = load_data(args.train)
+    X, y = split_feature_label(Xy)
+    X = transform(X)
+    model = train_model(X, y)
+
+    X = load_data(args.test)
+    passenger_ids = X['PassengerId']
+    X = transform(X)
+    y = model.predict(X)
+    submission = {
+        'PassengerId': passenger_ids,
+        'Survived': y
+    }
+    submission = pd.DataFrame(submission)
+    submission.to_csv('submission.csv', index=False)
+
+
+if __name__ == '__main__':
+    sys.exit(main(parse_args()))
